6199e20911354d58d5f38433bf93391d412f2b89
alextotalk <alexoo@inbox.ru>
2022-05-06 16:45:42 +0300

    useStat

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import {Header} from "./component/Header/Header";
 import {Body} from "./component/Body/Body";
 import {useId} from "react";
 import {Button} from "./component/Button/Button";
+import UseStat from "./component/UseStat/UseStat";
 
 function App() {
-    let students = [
-        {id: useId(), name: "Alex", age: 23},
-        {id: useId(), name: "Max", age: 25},
-        {id: useId(), name: "Bob", age: 20},
-        {id: useId(), name: "Mark", age: 33},
-    ]
-    let Foo1 = (subscriber: string) => {
-        alert(`Hi!!! ${subscriber}!!!`)
-    }
-    let Foo2 = (subscriber: string,age:number) => {
-        alert(`Hi!!! ${subscriber}!!! I am ${age} years old.`)
-    }
-    let Foo3 = () => {
-        alert(`Hi!!!`)
-    }
+    // let students = [
+    //     {id: useId(), name: "Alex", age: 23},
+    //     {id: useId(), name: "Max", age: 25},
+    //     {id: useId(), name: "Bob", age: 20},
+    //     {id: useId(), name: "Mark", age: 33},
+    // ]
+    // let Foo1 = (subscriber: string) => {
+    //     alert(`Hi!!! ${subscriber}!!!`)
+    // }
+    // let Foo2 = (subscriber: string,age:number) => {
+    //     alert(`Hi!!! ${subscriber}!!! I am ${age} years old.`)
+    // }
+    // let Foo3 = () => {
+    //     alert(`Hi!!!`)
+    // }
     return (
         <div className="App">
+            <UseStat/>
             {/*<Header title="Header"/>*/}
             {/*<Body title="List" students={students}/>*/}
             {/*<Button name={"Greeting"} callBack={() => Foo1("alex")}/>*/}
--- src/component/UseStat/UseStat.tsx
+++ src/component/UseStat/UseStat.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const UseStat = () => {
+
+    let [sum, setSum] = useState(0)
+
+    const addOne = () => {
+        setSum(++sum)
+        console.log(sum)
+    }
+    const reset = () => {
+        setSum(sum=0)
+     }
+
     return (
         <>
-          {/*<button onClick={}></button>*/}
+            <h1>{sum}</h1>
+            <button onClick={addOne}> push 1</button>
+            <button onClick={reset}>0</button>
         </>
     );
 };
